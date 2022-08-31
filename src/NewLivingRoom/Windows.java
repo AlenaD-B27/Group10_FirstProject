2a12be39aa0676c600ad08a9fd20a4782446a170
Zorig4Real <water.zorig@gmail.com>
2022-08-31 11:15:12 -0500

    windows is changed

--- src/NewLivingRoom/Windows.java
+++ src/NewLivingRoom/Windows.java
@@ -3,6 +3,8 @@ package NewLivingRoom;
 public class Windows {
 
     // change ths windows all glass with locals of inside
-    // take out all and lean it
+    // take out all and clean it
+    // change the all glass with frame with
+    //change the local windows frame
     
 }
