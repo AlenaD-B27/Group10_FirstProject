0db71897cd9a8fb411b507fc3952f4ee58537ea7
AlenaD-B27 <lenador.18@gmail.com>
2022-08-31 11:14:34 -0500

    I am hesitant about the prices

--- src/master_bathroom_alena/Shopping.java
+++ src/master_bathroom_alena/Shopping.java
@@ -5,4 +5,8 @@ public class Shopping {
     // buy white toilet bowl (up to $200)
     // buy sinks (white)
     // buy fittings (silver color)
+
+    // came to Ashley's
+    // omg so expensive
+    // still gonna buy stuff because I need it
 }
