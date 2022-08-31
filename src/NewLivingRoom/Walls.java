9d7d01d7baa73a91e1b7717c37b8a91452b588f7
Zorig4Real <water.zorig@gmail.com>
2022-08-31 11:09:28 -0500

    change the colors of Wall in New living Room

---
+++ src/NewLivingRoom/Walls.java
@@ -0,0 +1,12 @@
+package NewLivingRoom;
+
+public class Walls {
+    // Clean all side of walls
+    // Check it all electricity plugs
+    // fill out all wholes
+    // check it colors
+    
+
+
+
+}
